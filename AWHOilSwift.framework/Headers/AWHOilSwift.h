371a7c312b0b44f3b9a5f08b71dff51db49744d3
王恒 <1066026709@qq.com>
2025-07-28 23:00:20 +0800

    适配模拟器

--- AWHOilSwift.framework/Headers/AWHOilSwift.h
+++ AWHOilSwift.framework/Headers/AWHOilSwift.h
@@ -2,21 +2,16 @@
 //  AWHOilSwift.h
 //  AWHOilSwift
 //
-//  Created by 王恒 on 2022/10/9.
+//  Created by 王恒 on 2025/7/28.
 //
 
-#import <Foundation/Foundation.h>
-
-//! Project version number for AWHOilSwift.
-FOUNDATION_EXPORT double AWHOilSwiftVersionNumber;
-
-//! Project version string for AWHOilSwift.
-FOUNDATION_EXPORT const unsigned char AWHOilSwiftVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AWHOilSwift/PublicHeader.h>
+#ifndef AWHOilSwift_h
+#define AWHOilSwift_h
 
 #import <AWHOilSwift/AWHOSQuantityStatisticsDetailsModel.h>
 #import <AWHOilSwift/AWHOSOverallLineModel.h>
 #import <AWHOilSwift/AWHSSTemperatureHumidityDetailsModel.h>
 #import <AWHOilSwift/AWHSSQuantityStatisticsQueryCfgModel.h>
 #import <AWHOilSwift/AWHSSQuantityStatisticsQueryAdsModel.h>
+
+#endif /* AWHOilSwift_h */
